625b76b3624dc8494a8e19981686daa8bf758677
alisboa <alisboa@inegi.up.pt>
2021-10-26 12:19:10 +0100

    Mais uma vez

--- General/Convert_Equipments/Auxiliary/join_hx_and_technology.py
+++ General/Convert_Equipments/Auxiliary/join_hx_and_technology.py
@@ -60,7 +60,8 @@ def join_hx_and_technology(technologies,power_fraction,max_power_stream,max_powe
         'conversion_efficiency': conversion_efficiency,  # []
         'om_fix': om_fix / max_power_stream,# [€/year.kW]
         'om_var': om_var / max_power_stream,# [€/kWh]
-        'emissions': emissions  # [kg.CO2/kWh]
+        'emissions': emissions,  # [kg.CO2/kWh]
+        'tecnhologies': technologies,
 
     }
 
