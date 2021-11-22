9b0426b34c57b14c178d29734086173460799f69
alisboa <alisboa@inegi.up.pt>
2021-11-22 12:53:36 +0000

    a lot of intermal modificaitons; still updating/developing

---
+++ Source/simulation/Heat_Recovery/PINCH/Auxiliary/pretreatment.py
@@ -0,0 +1,134 @@
+
+
+def pretreatment(df_hot_streams, df_cold_streams, above_pinch,delta_T_min):
+
+    # initial value
+    split_needed = False
+    pairs = []
+    combinations_updated = []
+
+    # check if abvoe or below pinch
+    if above_pinch == True:
+        df_streams_in = df_hot_streams.copy()
+        df_streams_out = df_cold_streams.copy()
+    else:
+        df_streams_in = df_cold_streams.copy()
+        df_streams_out = df_hot_streams.copy()
+
+    # only perform this split when both df's have same stream number and there is a streams_in with larger mcp than all streams_out
+    if df_streams_in.shape[0] == df_streams_out.shape[0] :
+
+        # check if there is a stream in larger than all streams out
+        index_streams_in_to_split = []
+        for index,row in df_streams_in.iterrows():
+
+
+            if False in df_streams_out.apply(lambda x: True if x['mcp'] < row['mcp'] else False, axis=1).values:
+                pass
+            else:
+                split_needed = True
+                index_streams_in_to_split.append(index)
+
+        if split_needed == True:
+            # get first pair combinations between the streams to increase pinch analysis variability
+            for index_stream_out in df_streams_out.index.values:
+                for index_stream_in in index_streams_in_to_split:
+                    # only relevant to look for streams_out with larger mcp than streams_in
+                    if df_streams_in.loc[index_stream_in]['mcp'] > df_streams_out.loc[index_stream_out]['mcp']:
+                        # only relevant to look for streams_out whose temperature can meet streams_in temperature range
+                        if (df_streams_out.loc[index_stream_out]['Closest_Pinch_Temperature'] + delta_T_min < df_streams_in.loc[index_stream_in]['Supply_Temperature'] and above_pinch is True) or (  df_streams_out.loc[index_stream_out][ 'Closest_Pinch_Temperature'] - delta_T_min >  df_streams_in.loc[index_stream_in]['Supply_Temperature'] and above_pinch is False):
+                            pairs += [[index_stream_out, index_stream_in]]
+
+            for pair in pairs:
+                df_streams_in_copy = df_streams_in.copy()
+                df_streams_out_copy = df_streams_out.copy()
+                index_stream_out, index_stream_in = pair
+
+                if above_pinch == True:
+
+                    hot_stream_index = index_stream_in
+                    cold_stream_index = index_stream_out
+
+                    hx_cold_stream_T_cold = df_streams_out_copy.loc[index_stream_out]['Closest_Pinch_Temperature']
+                    cold_stream_max_T_hot = df_streams_out_copy.loc[index_stream_out]['Target_Temperature']
+                    cold_stream_mcp = df_streams_out_copy.loc[index_stream_out]['mcp']
+                    cold_stream = df_streams_out_copy.loc[index_stream_out]
+                    hot_stream = df_streams_in_copy.loc[index_stream_in]
+
+                    hot_stream_max_T_hot = df_streams_in_copy.loc[index_stream_in]['Supply_Temperature']
+                    hot_stream_T_cold = df_streams_in_copy.loc[index_stream_in]['Closest_Pinch_Temperature']
+                    hot_stream_mcp = df_streams_in_copy.loc[index_stream_in]['mcp']
+
+                    # Compute/Check Temperatures
+                    if hot_stream_max_T_hot >= (cold_stream_max_T_hot + delta_T_min):
+                        hx_cold_stream_T_hot = cold_stream_max_T_hot
+                    else:
+                        hx_cold_stream_T_hot = hot_stream_max_T_hot - delta_T_min
+
+                    # Split Stream in
+                    hx_power = cold_stream_mcp * (hx_cold_stream_T_hot - hx_cold_stream_T_cold)
+                    hx_hot_stream_T_hot = hot_stream_max_T_hot
+                    hx_hot_stream_T_cold = hot_stream_T_cold
+
+                    # Create Split Stream
+                    split_hot_stream_mcp = hx_power / (hx_hot_stream_T_hot - hx_hot_stream_T_cold)
+
+                    new_row = hot_stream.copy()  # split stream has same info as original
+                    new_row['mcp'] -= split_hot_stream_mcp  # correct split mcp
+                    new_row.name = str(int(hot_stream_index) * 100)  # new ID
+
+                    new_row['Split_Check'] = True
+
+                    # Add Split Stream to DFs
+                    df_streams_in_copy = df_streams_in_copy.append(new_row)
+
+                    # Update DFs
+                    df_streams_in_copy.loc[hot_stream_index, ['Split_Check']] = True
+                    df_streams_in_copy.loc[hot_stream_index, ['mcp']] = split_hot_stream_mcp
+
+                else:
+
+                    cold_stream_min_T_cold = df_streams_in.loc[index_stream_in]['Supply_Temperature']
+                    hot_stream_min_T_cold = df_streams_out.loc[index_stream_out]['Target_Temperature']
+                    hot_stream_T_hot = df_streams_out.loc[index_stream_out]['Supply_Temperature']
+                    hot_stream_mcp = df_streams_out.loc[index_stream_out]['mcp']
+                    cold_stream_T_hot = df_streams_in.loc[index_stream_in]['Closest_Pinch_Temperature']
+                    cold_stream = df_streams_in.loc[index_stream_in]
+                    cold_stream_index = index_stream_in
+
+                    # Compute/Check Temperatures
+                    if cold_stream_min_T_cold <= (hot_stream_min_T_cold - delta_T_min):
+                        hot_stream_T_cold = hot_stream_min_T_cold
+                    else:
+                        hot_stream_T_cold = cold_stream_min_T_cold + delta_T_min
+
+                    hx_power = hot_stream_mcp * (hot_stream_T_hot - hot_stream_T_cold)
+                    cold_stream_T_cold = cold_stream_min_T_cold
+
+                    # Create Split Stream
+                    split_stream_mcp = hx_power / (cold_stream_T_hot - cold_stream_T_cold)
+
+                    new_row = cold_stream.copy()  # split stream has same info
+                    new_row['mcp'] -= split_stream_mcp  # correct mcp
+                    new_row.name = str(int(cold_stream_index) * 100)  # new ID
+                    new_row['Split_Check'] = False
+
+                    # Add Split Stream to DFs
+                    df_streams_in = df_streams_in.append(new_row)
+
+                    # Update DFs
+                    df_streams_in.loc[index_stream_in]['Split_Check'] = True
+                    df_streams_in.loc[cold_stream_index, ['mcp']] = split_stream_mcp
+
+                combinations_updated.append([df_streams_in_copy, df_streams_out_copy])
+
+            all_combinations = combinations_updated
+
+        else:
+            all_combinations = [[df_hot_streams ,df_cold_streams]]
+
+
+    else:
+        all_combinations = [[df_hot_streams, df_cold_streams]]
+
+    return all_combinations
--- Source/simulation/Heat_Recovery/PINCH/Auxiliary/testing_above_pinch_make_pair.py
+++ Source/simulation/Heat_Recovery/PINCH/Auxiliary/testing_above_pinch_make_pair.py
@@ -2,60 +2,102 @@ from ......Source.simulation.Heat_Recovery.PINCH.Above_Pinch.above_pinch_hx_temp
 from ......Source.simulation.Heat_Recovery.PINCH.HX.pinch_design_hx import pinch_design_hx
 from ......Source.simulation.Heat_Recovery.PINCH.Above_Pinch.above_pinch_stream_info import above_pinch_stream_info
 from copy import deepcopy
-
+import numpy as np
 
 def main_above_pinch_make_pair(combination,delta_T_min):
 
     all_combinations = []
+
     all_combinations = above_pinch_make_pair(combination,all_combinations,delta_T_min)
+
+
+    # eliminate repetitions
+
+    if len(all_combinations) > 1:
+        keep = []
+        keep.append(all_combinations[0])
+        for i in all_combinations:
+            append = True
+
+            i_copy = i[2].copy()
+            i_copy = i_copy.sort_values('HX_Turnkey_Cost')
+
+            i_copy.index = np.arange(1, len(i_copy) + 1)
+
+            for j in keep:
+                j_copy = j[2].copy()
+                j_copy = j_copy.sort_values('HX_Turnkey_Cost')
+                j_copy.index = np.arange(1, len(j_copy) + 1)
+
+                if i_copy[['Power','HX_Turnkey_Cost']].equals(j_copy[['Power','HX_Turnkey_Cost']]) != True and append == True:
+                    append = True
+                else:
+                    append = False
+
+            if append == True:
+                keep.append(i)
+
+        all_combinations = keep
+
 
     return all_combinations
 
 
-def above_pinch_make_pair(combination,all_combinations,delta_T_min):
-    combination_copy = deepcopy(combination)
 
+def above_pinch_make_pair(combination,all_combinations,delta_T_min):
+
+    # defined var
     above_pinch = True
 
-    df_hot_streams, df_cold_streams, df_hx = deepcopy(combination_copy.copy())
+    # get streams and hx df's
+    combination_copy = deepcopy(combination)
 
+    # get info to make pair matches
+    df_hot_streams,df_cold_streams, df_hx = deepcopy(combination_copy.copy())
+
+    # get streams which were not yet matched
     df_hot_streams_for_index = df_hot_streams[df_hot_streams['Match'] == False].copy()
     df_cold_streams_for_index = df_cold_streams[df_cold_streams['Match'] == False].copy()
+
+
 
     # get first pair combinations between the streams to increase pinch analysis variability
     for cold_stream_index in df_cold_streams_for_index.index.values:
 
+        # save cold stream random value for iterations
         save_index_here = deepcopy(-200)
 
-        df_hot_streams, df_cold_streams, df_hx = deepcopy(combination_copy.copy())
+        # save heat stream random value for iterations
+        save_index = deepcopy(-100)
+
+        # when iterating the df's must be received again, since they are changing
+        df_hot_streams, df_cold_streams,  df_hx = deepcopy(combination_copy.copy())
         df_hx_copy = df_hx.copy()
         df_cold_streams.loc[cold_stream_index, 'Match'] = False
 
-        save_index = deepcopy(-100)
         for hot_stream_index in df_hot_streams_for_index.index.values:
 
-
                 if save_index_here != -200:
+                    # when iterating the cold stream must be received again
                     df_cold_streams.loc[save_index_here] = deepcopy((combination_copy[1].loc[save_index_here]).copy())
                     df_cold_streams.loc[save_index_here, 'Match'] = False
 
                 if save_index != -100:
+                    # when iterating the hot stream must be received again
                     df_hot_streams.loc[save_index] = deepcopy((combination_copy[0].loc[save_index]).copy())
                     df_hot_streams.loc[save_index, 'Match'] = False
+
 
                 df_cold_streams.loc[cold_stream_index, 'Match'] = False
                 df_hot_streams.loc[hot_stream_index, 'Match'] = False
 
+
                 # look for streams_out with larger mcp than streams_in if number_cold_streams = number_hot_streams
                 # and for all streams when number_cold_streams < number_hot_streams
-
-                # ORIGINAL IF
-               #if (df_cold_streams.loc[cold_stream_index]['mcp'] >= df_hot_streams.loc[hot_stream_index]['mcp'] and df_cold_streams.shape[0] == df_hot_streams.shape[0]):
-
-                if (df_cold_streams.loc[cold_stream_index]['mcp'] >= df_hot_streams.loc[hot_stream_index]['mcp'] and df_cold_streams.shape[0] == df_hot_streams.shape[0]) or (df_cold_streams.loc[cold_stream_index]['mcp'] <= df_hot_streams.loc[hot_stream_index]['mcp'] and df_cold_streams.shape[0] > df_hot_streams.shape[0]):
+                if (df_cold_streams.loc[cold_stream_index]['mcp'] >= df_hot_streams.loc[hot_stream_index]['mcp'] and df_cold_streams[df_cold_streams['Reach_Pinch'] == True].shape[0] == df_hot_streams[df_hot_streams['Reach_Pinch'] == True ].shape[0]) or (df_cold_streams.loc[cold_stream_index]['mcp'] <= df_hot_streams.loc[hot_stream_index]['mcp'] and df_cold_streams.shape[0] > df_hot_streams.shape[0]):
                     # only relevant to look for streams_out whose temperature can meet streams_in temperature range
-                    if (df_cold_streams.loc[cold_stream_index]['Closest_Pinch_Temperature'] + delta_T_min < df_hot_streams.loc[hot_stream_index]['Supply_Temperature'] and above_pinch is True) or ( df_cold_streams.loc[cold_stream_index]['Closest_Pinch_Temperature'] - delta_T_min > df_hot_streams.loc[hot_stream_index]['Supply_Temperature'] and above_pinch is False):
-
+                    if (df_cold_streams.loc[cold_stream_index]['Closest_Pinch_Temperature'] + delta_T_min < df_hot_streams.loc[hot_stream_index]['Supply_Temperature'] and above_pinch == True) or ( df_cold_streams.loc[cold_stream_index]['Closest_Pinch_Temperature'] - delta_T_min > df_hot_streams.loc[hot_stream_index]['Supply_Temperature'] and above_pinch is False):
+                       # only looking to streams which reach pinch
                         if df_cold_streams.loc[cold_stream_index]['Reach_Pinch'] == True and df_hot_streams.loc[hot_stream_index]['Reach_Pinch'] == True:
 
                             # Hot Stream Max mcp
@@ -109,13 +151,7 @@ def above_pinch_make_pair(combination,all_combinations,delta_T_min):
                                     # Update DFs
                                     df_hot_streams.loc[hot_stream_index, ['mcp']] = split_hot_stream_mcp
 
-                                ###############################
-                                ###############################
 
-                                # ITS MULTYPLYNG STREAMS
-
-                                ###############################
-                                ###############################
 
 
                                 save_index = deepcopy(hot_stream_index)
@@ -139,14 +175,18 @@ def above_pinch_make_pair(combination,all_combinations,delta_T_min):
                                 combination = [df_hot_streams,df_cold_streams,df_hx]
 
 
-                                if df_hot_streams[df_hot_streams['Match'] == False].shape[0] > 0:
+
+                                if df_hot_streams[(df_hot_streams['Match'] == False) & (df_hot_streams['Reach_Pinch'] == True)].shape[0] > 0:
+
                                     all_combinations = above_pinch_make_pair(deepcopy(combination.copy()), deepcopy(all_combinations),delta_T_min)
+
+                                    df_hx.drop(df_hx.tail(1).index,inplace=True)
+
+
                                 else:
 
-                                    df_hx = df_hx.drop_duplicates(subset=['Original_Hot_Stream','Hot_Split'], keep='last',)
-                                    df_hx = df_hx.drop_duplicates(subset=['Original_Cold_Stream','Cold_Split'], keep='last',)
-
                                     df_hx.drop(columns=['Hot_Split','Cold_Split'],inplace=True)
+
                                     all_combinations.append(deepcopy([df_hot_streams,df_cold_streams,df_hx]))
 
 
