035cc2eab886378ad2231b84b9dbb776d7047a80
alisboainegi <alisboa@inegi.up.pt>
2022-05-20 11:57:24 +0100

    1)

--- Source/simulation/Heat_Recovery/Pinch/make_pinch_design_draw.py
+++ Source/simulation/Heat_Recovery/Pinch/make_pinch_design_draw.py
@@ -1,29 +1,7 @@
-
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, mpld3
 import matplotlib.lines as mlines
 
 def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta_temperature):
-
-    ################################################################
-
-    ## get streams info - look on stream at a time a check above/below pinch
-    #for stream_info in streams_info:
-    #    if len(stream_info['above_pinch']) > 1:
-    #        i = 0
-    #        for index_above, above_info in enumerate(stream_info['above_pinch']):
-    #            if above_info['id'] == stream_info['id']:
-    #                i += 1
-    #                if i == 2:
-    #                    stream_info['above_pinch'].pop(index_above)
-#
-    #    if len(stream_info['below_pinch']) > 1:
-    #        i = 0
-    #        for index_below, below_info in enumerate(stream_info['below_pinch']):
-    #            if below_info['id'] == stream_info['id']:
-    #                i += 1
-    #                if i == 2:
-    #                    stream_info['below_pinch'].pop(index_below)
-#
 
     ###########################
     # Defined vars
@@ -57,7 +35,7 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
     x_min = 0
     y_min = 0
     y_max = space_between_streams * (number_streams + 1)
-    x_max = y_max * 1.5
+    x_max = y_max * 2
     pinch = x_max / 2
 
     ######################################################################################
@@ -114,7 +92,9 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
         # hot streams
         if supply_temperature > target_temperature:
             # write stream ID
-            plt.gca().text(x_min - 4, y_stream, str(stream['id']),size='large', style='normal',bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10}, weight='bold', ha='right', va='center')
+            # manual bold
+            plt.gca().text(x_min - 4, y_stream, str(stream['id']),color="red", size='large', style='normal', weight='bold', ha='right', va='center')
+            plt.gca().text(x_min - 4, y_stream, str(stream['id']), color="red", size='large', style='normal', weight='bold', ha='right', va='center')
 
             if supply_temperature > pinch_temperature:
                 left_temperature = x_min
@@ -130,8 +110,8 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
             else:
                 right_temperature = pinch-(left_side_pinch.index(target_temperature) + 1) * 3
 
-            plt.gca().text(left_temperature + 0.5 , y_stream+height_temperature_text, "{:.1f}".format(supply_temperature) + 'º', ha="left", va="bottom")   # draw left temperature
-            plt.gca().text(right_temperature - 2.5, y_stream+height_temperature_text, "{:.1f}".format(target_temperature)+ 'º', ha="left", va="bottom")   # draw right temperature
+            plt.gca().text(left_temperature + 0.5 , y_stream+height_temperature_text, "{:.0f}".format(supply_temperature) + 'º', ha="left", va="bottom")   # draw left temperature
+            plt.gca().text(right_temperature - 3, y_stream+height_temperature_text, "{:.0f}".format(target_temperature)+ 'º', ha="left", va="bottom")   # draw right temperature
             rectangle = plt.Rectangle((left_temperature-0.1, y_stream-1), 0.2, 2, color='r')
             plt.gca().add_patch(rectangle)  # draw beginning of stream
 
@@ -165,10 +145,9 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                         i += 1
 
             # SPLIT BELOW PINCH
-
             if len(stream['below_pinch']) > 1:
                 i = 0
-                for iterator in range(len(stream['below_pinch']) - 1):
+                for iterator in range(len(stream['below_pinch'])): # for iterator in range(len(stream['below_pinch']) - 1):
                     if stream['below_pinch'][iterator]['id'] != stream['id']:
                         if supply_temperature > pinch_temperature:
                             split_closest_pinch_temperature = left_temperature
@@ -187,7 +166,10 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
         # cold streams
         else:
             # write stream ID
-            plt.gca().text(x_max + 4, y_stream, str(stream['id']), size='large', style='normal', bbox={'facecolor': 'blue', 'alpha': 0.5, 'pad': 10}, weight='bold', ha='left', va='center')
+            # manual bold
+            plt.gca().text(x_max + 4, y_stream, str(stream['id']),color="blue", size='large', style='normal', bbox={'facecolor': 'blue', 'alpha': 0.5, 'pad': 10}, weight='bold', ha='left', va='center')
+            plt.gca().text(x_max + 4, y_stream, str(stream['id']),color="blue", size='large', style='normal', bbox={'facecolor': 'blue', 'alpha': 0.5, 'pad': 10}, weight='bold', ha='left', va='center')
+
             dict_for_hx[str(stream['id'])] = y_stream
 
             if supply_temperature == pinch_temperature-pinch_delta_temperature:
@@ -205,8 +187,8 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                 left_temperature = pinch + (right_side_pinch.index(target_temperature) + 1) * 3
 
             # write temperatures
-            plt.gca().text(left_temperature + 2.4 , y_stream+height_temperature_text, "{:.2f}".format(target_temperature)+ 'º', ha="right", va="bottom")
-            plt.gca().text(right_temperature - 0.8, y_stream+height_temperature_text, "{:.2f}".format(supply_temperature)+ 'º', ha="right", va="bottom")
+            plt.gca().text(left_temperature + 3, y_stream+height_temperature_text, "{:.0f}".format(target_temperature)+ 'º', ha="right", va="bottom")
+            plt.gca().text(right_temperature - 0.8, y_stream+height_temperature_text, "{:.0f}".format(supply_temperature)+ 'º', ha="right", va="bottom")
 
             # draw arrows
             rectangle = plt.Rectangle((right_temperature-0.1, y_stream-1), 0.2, 2, color='b')
@@ -245,8 +227,6 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                 i = 0
                 for iterator in range(len(stream['below_pinch'])):
                     if stream['below_pinch'][iterator]['id'] != stream['id']:
-
-                        print("target_temperature",target_temperature,"pinch_temperature", pinch_temperature)
                         if target_temperature > pinch_temperature - pinch_delta_temperature:
                             split_closest_pinch_temperature = pinch
                         else:
@@ -272,6 +252,10 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
 
     # design HX above pinch
     if len(above_pinch_hx) > 0:
+
+        #sort HX by temperatures
+        info_to_design_hx['above_pinch'] = sorted(info_to_design_hx['above_pinch'], key=lambda d: d['HX_Cold_Stream_T_Hot'])
+
         for match in info_to_design_hx['above_pinch']:
             plt.plot((go_to_the_left, go_to_the_left), (dict_for_hx[str(match['HX_Cold_Stream'])], dict_for_hx[str(match['HX_Hot_Stream'])]), 'k-')
 
@@ -292,7 +276,7 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                     if round(pinch_temperature) != round(match['HX_Hot_Stream_T_Cold']) and round(stream_info['temperatures'][1]) != round(match['HX_Hot_Stream_T_Cold']):
                         plt.gca().text(go_to_the_left + 1,
                                        dict_for_hx[str(match['HX_Hot_Stream'])] + height_temperature_text ,
-                                       "{:.1f}".format(round(match['HX_Hot_Stream_T_Cold'])) + 'º', ha="left", va="bottom")
+                                       "{:.0f}".format(round(match['HX_Hot_Stream_T_Cold'])) + 'º', ha="left", va="bottom")
                         break
 
             # write cold streams hx temperatures
@@ -301,7 +285,7 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                     if round(stream_info['temperatures'][1]) != round(match['HX_Cold_Stream_T_Hot']):
                         plt.gca().text(go_to_the_left - 1,
                                        dict_for_hx[str(match['HX_Cold_Stream'])] + height_temperature_text ,
-                                       "{:.1f}".format(round(match['HX_Cold_Stream_T_Hot'])) + 'º', ha="right", va="bottom")
+                                       "{:.0f}".format(round(match['HX_Cold_Stream_T_Hot'])) + 'º', ha="right", va="bottom")
                         break
 
             go_to_the_left -= left_movement
@@ -309,7 +293,12 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
     # design HX below pinch
     right_movement = (x_max / 2) / (len(below_pinch_hx) + 2)
     go_to_the_right = x_max / 2 + right_movement
+
     if len(below_pinch_hx) > 0:
+
+        # sort HX by temperatures
+        info_to_design_hx['below_pinch'] = sorted(info_to_design_hx['below_pinch'], key=lambda d: d['HX_Hot_Stream_T_Cold'], reverse=True)
+
         for match in info_to_design_hx['below_pinch']:
             plt.plot((go_to_the_right, go_to_the_right), (dict_for_hx[str(match['HX_Cold_Stream'])], dict_for_hx[str(match['HX_Hot_Stream'])]), 'k-')
             circle_hot = plt.Circle((go_to_the_right, dict_for_hx[str(match['HX_Hot_Stream'])]), circle_radius, color='black', zorder=5)
@@ -329,7 +318,7 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                     if round(stream_info['temperatures'][1]) != match['HX_Hot_Stream_T_Cold']:
                         plt.gca().text(go_to_the_right + 1,
                                        dict_for_hx[str(match['HX_Hot_Stream'])] + height_temperature_text ,
-                                       "{:.1f}".format(round(match['HX_Hot_Stream_T_Cold'])) +'º', ha="left", va="bottom")
+                                       "{:.0f}".format(round(match['HX_Hot_Stream_T_Cold'])) +'º', ha="left", va="bottom")
 
                         break
 
@@ -339,7 +328,7 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                     if round(pinch_temperature-pinch_delta_temperature) != round(match['HX_Cold_Stream_T_Hot']) and round(stream_info['temperatures'][1]) != round(match['HX_Cold_Stream_T_Hot']):
                         plt.gca().text(go_to_the_right - 1,
                                        dict_for_hx[str(match['HX_Cold_Stream'])] + height_temperature_text ,
-                                       "{:.1f}".format(round(match['HX_Cold_Stream_T_Hot'])) + 'º', ha="right", va="bottom")
+                                       "{:.0f}".format(round(match['HX_Cold_Stream_T_Hot'])) + 'º', ha="right", va="bottom")
                         break
 
             go_to_the_right += right_movement
@@ -367,13 +356,19 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
         stream_y = dict_for_hx[str(stream['id'])]
 
         if str(stream['id']) in dict_temperatures.keys():
-            if len(stream['above_pinch']) > 1:
+
+            id_list = [i['id'] for i in stream['above_pinch']]
+            if len(list(set(id_list))) > 1:
+                no_split_exist = True
+            else:
+                no_split_exist = False
+
+            if len(stream['above_pinch']) > 1 and no_split_exist == True:
                     # get temperature the stream reaches
                     for key in dict_temperatures[str(stream['id'])].keys():
                         write_temperature += dict_temperatures[str(stream['id'])][key]['temperature'] * dict_temperatures[str(stream['id'])][key]['mcp'] /mcp_total
 
-                    plt.gca().text(diagonal_space_cold_stream_out, stream_y + height_temperature_text, "{:.1f}".format(write_temperature) + 'º',ha='center',va='bottom')
-
+                    plt.gca().text(diagonal_space_cold_stream_out, stream_y + height_temperature_text, "{:.0f}".format(write_temperature) + 'º',ha='center',va='bottom')
                     # get utility power
                     power_utility = mcp_total * (target_temperature-write_temperature)
 
@@ -395,7 +390,7 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
                         plt.gca().text(diagonal_space_cold_stream_out / 2, stream_y, 'H', weight='bold', ha='center', va='center', zorder=15)
                         plt.gca().text(diagonal_space_cold_stream_out / 2, stream_y - circle_radius - 1, str(round(power_utility, 1)) + 'kW', ha='center', va='center')
 
-        elif  target_temperature > supply_temperature and target_temperature > pinch_temperature - pinch_delta_temperature:
+        elif target_temperature > supply_temperature and target_temperature > pinch_temperature - pinch_delta_temperature:
 
             power_utility = mcp_total * (target_temperature - (pinch_temperature - pinch_delta_temperature))
 
@@ -429,12 +424,20 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
         stream_y = dict_for_hx[str(stream['id'])]
 
         if str(stream['id']) in dict_temperatures.keys():
-            if len(stream['below_pinch']) > 1:
+
+            id_list = [i['id'] for i in stream['below_pinch']]
+            if len(list(set(id_list))) > 1:
+                no_split_exist = True
+            else:
+                no_split_exist = False
+
+
+            if len(stream['below_pinch']) > 1 and no_split_exist == True:
                     for key in dict_temperatures[str(stream['id'])].keys():
                         write_temperature += dict_temperatures[str(stream['id'])][key]['temperature'] * dict_temperatures[str(stream['id'])][key]['mcp'] / mcp_total
 
                     plt.gca().text(x_max-diagonal_space_hot_stream_out, stream_y + height_temperature_text,
-                                   "{:.1f}".format(write_temperature) + 'º', ha='center', va='bottom')
+                                   "{:.0f}".format(write_temperature) + 'º', ha='center', va='bottom')
 
                     power_utility = mcp_total * abs(target_temperature - write_temperature)
 
@@ -472,7 +475,6 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
 
             power_utility = mcp_total * (pinch_temperature - target_temperature)
 
-
             if power_utility > 0:
                 circle_hot_utility = plt.Circle((x_max - (diagonal_space_hot_stream_out / 2), stream_y), circle_radius,
                                                 color='blue', zorder=5)
@@ -487,24 +489,20 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
 
     ########################################################################
     # PINCH LINE AND TEMPERATURES
-    plt.plot((pinch, pinch), (y_min, y_max), 'k--')
-    plt.gca().text(pinch, y_max+5, '- PINCH ('+"{:.1f}".format(pinch_temperature - pinch_delta_temperature/2) + 'ºC) -',weight='bold', ha='center', va='top',size='large')
-    plt.gca().text(pinch, y_max, "{:.1f}".format(pinch_temperature) + 'ºC', style='italic', ha='right', va='bottom')
-    plt.gca().text(pinch, y_min - 1, "{:.1f}".format(pinch_temperature-pinch_delta_temperature) + 'ºC', style='italic', ha='left', va='top')
+    plt.plot((pinch, pinch), (y_min+ space_between_splits/2, y_max- space_between_splits/2), 'k--')
+    plt.gca().text(pinch, y_max - space_between_splits/2, "{:.1f}".format(pinch_temperature) + 'ºC', style='italic', ha='right', va='bottom')
+    plt.gca().text(pinch, y_min, "{:.1f}".format(pinch_temperature-pinch_delta_temperature) + 'ºC', style='italic', ha='left', va='top')
 
-    # plot cp
-    #plt.gca().text(x_max + 13, y_max+3, 'Original Streams', ha='center', va='center',size='large')
-#
+    #plt.title('- HX NETWORK - PINCH ANALYSIS ('+"{:.1f}".format(pinch_temperature - pinch_delta_temperature/2) + 'ºC)',weight='bold', ha='center', va='top',size='large')
+
+    # DRAW MCP
+    #plt.gca().text(x_max + 13, y_max+3, 'Original Streams', ha='center', va='center',size='large')#
     #plt.gca().text(x_max + 13, y_max+1, '$mc_p$', weight='bold', ha='center', va='center',size='large')
     #plt.gca().text(x_max + 13, y_max-1, '[kJ/K]', ha='center', va='center')
-    #plt.gca().add_patch(plt.Rectangle((x_max+7.5, y_min), 11, (y_max-y_min+5), alpha=0.2, edgecolor='black', facecolor='silver',clip_on=False, linewidth=0.5))
-#
+    #plt.gca().add_patch(plt.Rectangle((x_max+7.5, y_min), 11, (y_max-y_min+5), alpha=0.2, edgecolor='black', facecolor='silver',clip_on=False, linewidth=0.5))#
     #for stream in streams_info:
     #    plt.gca().text(x_max + 13, dict_for_hx[str(stream['id'])], "{:.2f}".format(stream['mcp']), ha='center', va='center',size='large')
-#
 
-    ########################################################################
-    # EXTRA
     # plot axis limits
     plt.gca().set_xlim(x_min-1, x_max+1)
     plt.gca().set_ylim(y_min, y_max)
@@ -517,15 +515,58 @@ def make_pinch_design_draw(streams_info,pinch_temperature,pinch_data,pinch_delta
     plt.tight_layout()
 
     # legend
-    blue_line = mlines.Line2D([], [], color='blue', marker='_',markersize=15, label='Cold stream')
-    red_line = mlines.Line2D([], [], color='red', marker='_',markersize=15, label='Hot stream')
-    circle_hx = mlines.Line2D([0], [0], color='black', markerfacecolor='white', marker='o',markersize=15, label='Heat exchanger')
-    circle_hot_utility = mlines.Line2D([0], [0], color='red', markerfacecolor='white', marker='o',markersize=15, label='Hot utility')
-    circle_cold_utility = mlines.Line2D([0], [0], markeredgecolor='blue', markerfacecolor='white', marker='o',markersize=15, label='Cold utility')
-    handles = [circle_hx,blue_line,circle_hot_utility,red_line,circle_cold_utility]
+    blue_line = mlines.Line2D([], [],
+                              color='blue',
+                              marker='_',
+                              markersize=15,
+                              label='Cold stream')
 
-    plt.legend(handles=handles,loc="lower right", ncol=3)
-    plt.show()
+    red_line = mlines.Line2D([], [],
+                             color='red',
+                             marker='_',
+                             markersize=15,
+                             label='Hot stream')
+
+    circle_hx = mlines.Line2D([0], [0], color='black',
+                              markerfacecolor='white',
+                              marker='o',
+                              markersize=15,
+                              label='Heat exchanger')
+
+    circle_hot_utility = mlines.Line2D([0], [0],
+                                       color='red',
+                                       markerfacecolor='white',
+                                       marker='o',
+                                       markersize=15,
+                                       label='Hot utility')
+
+    circle_cold_utility = mlines.Line2D([0], [0],
+                                        markeredgecolor='blue',
+                                        markerfacecolor='white',
+                                        marker='o',
+                                        markersize=15,
+                                        label='Cold utility')
+
+    handles = [circle_hx, blue_line, circle_hot_utility, red_line, circle_cold_utility]
+
+    plt.legend(handles=handles, loc="lower right", ncol=3)
+
+    # adjust plot size
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+    fig = plt.gcf()
+    coef = 6 / 16
+    y_val = 14
+    fig.set_size_inches(y_val, coef * y_val)
+    ax = plt.gca()
+    ax.set_xlim(x_min - 15, x_max + 15)
+    ax.set_ylim(y_min - 5, y_max + 5)
+
+    # get HTML
+    fig_html = mpld3.fig_to_html(fig)
+    plt.close()
+
+    return fig_html
 
 
 
