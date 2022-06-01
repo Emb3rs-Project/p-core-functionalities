8f4881d4cd5f6a08fffdc63a8e5121d6252fc574
alisboainegi <alisboa@inegi.up.pt>
2022-06-01 15:05:20 +0100

    UPDATE

--- Error_Handling/General/simple_industry_stream_data_input.py
+++ Error_Handling/General/simple_industry_stream_data_input.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, validator, conint, conlist, PositiveFloat, StrictStr, NonNegativeFloat
+from pydantic import BaseModel, validator, conlist, PositiveFloat, StrictStr, NonNegativeFloat
 from typing import Optional
 from enum import Enum
 import ast
@@ -13,8 +13,8 @@ class SimpleIndustryStreamDataInput(BaseModel):
     supply_temperature: PositiveFloat
     target_temperature: PositiveFloat
     fluid: StrictStr
-    fluid_cp: Optional[PositiveFloat] = None
-    flowrate: Optional[PositiveFloat] = None
+    fluid_cp: Optional[PositiveFloat]
+    flowrate: Optional[PositiveFloat]
     daily_periods: Optional[str]
     shutdown_periods: Optional[str]
     saturday_on: Optional[ScheduleInfo]
@@ -68,10 +68,11 @@ class SimpleIndustryStreamDataInput(BaseModel):
 
         if values["fluid"] == 'steam' and capacity == None:
             raise Exception('When introducing steam as a fluid, introduce the capacity.')
-
         elif (capacity == None and values['flowrate'] == None and values['fluid_cp'] == None):
             raise Exception('To characterize a stream, introduce Capacity or Flowrate and Cp.')
         elif (capacity == None and (values['flowrate'] == None or values['fluid_cp'] == None)):
+            raise Exception('To characterize a stream, introduce Capacity or Flowrate and Cp.')
+        elif (capacity != None and values['flowrate'] != None and values['fluid_cp'] != None):
             raise Exception('To characterize a stream, introduce Capacity or Flowrate and Cp.')
         else:
             return capacity
